dbdc7bf370dee32d564f5e5ec538076967ce6935
Sebastian Werner <s.werner@sebastian-software.de>
2021-12-21 21:23:24 +0100

    Formatting + added prettier

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,10 @@
 module.exports = {
   targets: 'current node',
   presets: [
-    ['@babel/preset-env', {modules: 'commonjs', useBuiltIns: 'entry',corejs:3,bugfixes: true}],
-    '@babel/preset-typescript'
-
-
+    [
+      '@babel/preset-env',
+      {modules: 'commonjs', useBuiltIns: 'entry', corejs: 3, bugfixes: true},
+    ],
+    '@babel/preset-typescript',
   ],
 };
